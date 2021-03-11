aa57981c53daddcf94b3cd2fcbece1d7c52b6307
jewelran <mdjewelrana6487@gmail.com>
2021-03-12 02:35:01 +0600

    add somthing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>hello , world , welcome to our site</h1>
+        <h3>my name is jewel rana, font end web developer</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
